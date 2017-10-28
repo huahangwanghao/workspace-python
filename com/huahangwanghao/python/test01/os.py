a06b3c46c683e878129dcc2e86ae739785f153c5
wanghao2 <wanghao2@gomeholdings.com>
2017-10-28 14:48:24 +0800

    读取配置文件

--- com/huahangwanghao/python/test01/os.py
+++ com/huahangwanghao/python/test01/os.py
@@ -1,6 +1,26 @@
-#coding:utf-8
+#encoding:utf-8
 import os
-from email.mime.text import  MIMEText
-import configreader
+import configparser
 
-os.system("ping www.baidu.com")
+
+f=open("conf.txt",'r')
+cmd=f.readline()
+#os.system(cmd)
+
+#encoding:utf-8
+#name:mod_config.py
+
+
+#获取config配置文件
+def getConfig(section, key):
+    config = configparser.ConfigParser()
+    print("当前目录:",os.path.split(os.path.realpath(__file__))[0])
+    path = os.path.split(os.path.realpath(__file__))[0] + '/db.conf'
+    config.read(path,encoding='utf-8')
+    return config.get(section, key)
+
+#其中 os.path.split(os.path.realpath(__file__))[0] 得到的是当前文件模块的目录
+
+dbname = getConfig("database", "dbname")
+
+print(dbname)
